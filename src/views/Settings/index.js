1f70b1347b707c009584d247c4d43b89afd036ba
Juan Carlos Rodriguez Ample <jcrodriguezam@outlook.es>
2021-05-30 23:52:07 +0200

    Configure firebase

--- src/views/Settings/index.js
+++ src/views/Settings/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { makeStyles, Grid } from '@material-ui/core';
-import { StylesProvider, useTheme } from '@material-ui/core/styles';
+import { useTheme } from '@material-ui/core/styles';
 import { useSelector, useDispatch } from 'react-redux';
 import * as actionTypes from '../../store/actions';
 
